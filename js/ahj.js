11ba9bf73742246941c38b46111516f9b4c2be6f
zun5761 <zun5761@gmail.com>
2022-10-13 16:41:03 +0900

    slide 주석 제거

--- js/ahj.js
+++ js/ahj.js
@@ -10,15 +10,11 @@ function getStyle(element, attr) {
 }
 
 function getSiblings(e) {
-  // for collecting siblings
   const siblings = [];
-  // if no parent, return no sibling
   if (!e.parentNode) {
     return siblings;
   }
-  // first child of the parent node
   let sibling = e.parentNode.firstChild;
-  // collecting siblings
   while (sibling) {
     if (sibling.nodeType === 1 && sibling !== e) {
       siblings.push(sibling);
