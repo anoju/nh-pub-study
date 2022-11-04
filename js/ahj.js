76d64b56ec758fa2dc6cb7b284646cffd0663565
zun5761 <zun5761@gmail.com>
2022-11-04 09:54:03 +0900

    scrollto

--- js/ahj.js
+++ js/ahj.js
@@ -143,18 +143,18 @@ Math.easeInOutQuad = function (t, b, c, d) {
 };
 
 Math.easeInCubic = function (t, b, c, d) {
-  var tc = (t /= d) * t * t;
+  const tc = (t /= d) * t * t;
   return b + c * tc;
 };
 
 Math.inOutQuintic = function (t, b, c, d) {
-  var ts = (t /= d) * t,
-    tc = ts * t;
+  const ts = (t /= d) * t;
+  const tc = ts * t;
   return b + c * (6 * tc * ts + -15 * ts * ts + 10 * tc);
 };
 
 // requestAnimationFrame for Smart Animating http://goo.gl/sx5sts
-var requestAnimFrame = (function () {
+const requestAnimFrame = (function () {
   return (
     window.requestAnimationFrame ||
     window.webkitRequestAnimationFrame ||
